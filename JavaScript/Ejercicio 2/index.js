e5161fe057643a33a866cf4bd8c493d0af63ee42
Fernando Espericueta <fernandostark18@gmail.com>
2022-11-21 15:36:09 -0600

    Solución del ejercicio 2

---
+++ JavaScript/Ejercicio 2/index.js
@@ -0,0 +1,16 @@
+/*
+Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+- Tu nombre (string)
+- Tu edad (number)
+- ¿Eres desarrollador? (boolean)
+- Tu fecha de nacimiento (Date)
+- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url) */
+
+const lst = [
+    "Fernando", 22, true, new Date("September 05 2000"), {
+        titulo: "El Psicoanalista",
+        autor: "John Katzenbach",
+        anyo: "2002",
+        url: "https://www.amazon.com.mx/El-psicoanalista-John-Katzenbach/dp/6075293728/ref=asc_df_6075293728/?tag=gledskshopmx-20&linkCode=df0&hvadid=295440059942&hvpos=&hvnetw=g&hvrand=14653796384453221791&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1010124&hvtargid=pla-432435886510&psc=1"
+    }
+];
