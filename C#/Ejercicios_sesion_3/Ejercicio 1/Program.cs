340f28199d43b0c41cc3577f4928139f54068ccc
Fernando Espericueta <fernandostark18@gmail.com>
2023-01-06 20:26:36 -0600

    Ejercicio de la sesión 3 resuelto

---
+++ C#/Ejercicios_sesion_3/Ejercicio 1/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ Ejercicio 1
+
+ Crea una estructura de datos para un cliente con estos campos:
+ Nombre completo
+ Teléfono
+ Dirección
+ Email
+ Si es nuevo cliente
+ Bonus: escribe un método para presentar estos datos desde la estructura al hacer Console.WriteLine(...)
+ */
+Cliente Cliente_1 = new Cliente("Fernando Espericueta", "316 186 62 41", "Emiliano Zapata #85", "cliente1@correo.com", true);
+Console.WriteLine(Cliente_1);
+public struct Cliente
+{
+    public Cliente(string fullName, string phone, string address, string email, bool isNew)
+    {
+        FullName = fullName;
+        Phone = phone;
+        Address = address;
+        Email = email;
+        IsNew = isNew;
+    }
+
+    public string FullName { get; }
+    public string Phone { get; }
+    public string Address { get; }
+    public string Email { get; }
+    public bool IsNew { get; }
+
+    public override string ToString() => $"({FullName}, {Phone}, {Address}, {Email}, {IsNew})";
+}
