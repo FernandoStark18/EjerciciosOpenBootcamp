f12e2af7b0a5f5a21b2c24c2625b85c70cbe1552
Fernando Espericueta <fernandostark18@gmail.com>
2023-01-31 00:18:08 -0600

    My first backend app

---
+++ APIs con .NET/Nueva carpeta/University-API-Backend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using University_API_Backend.Models.DataModels;
+
+namespace University_API_Backend.DataAccess
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options) 
+        {
+
+        }
+
+        // To do: Add DbSets (tables of our database)
+        public DbSet<User>? Users { get; set; }
+
+    }
+}
